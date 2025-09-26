44377dd965430169de963d318262c933f81ee78a
saqishah444 <syedsaqibbukhari055@gmail.com>
2025-09-26 17:17:00 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
       {/* Home Section */}
       <section
         id="membership"
-        className="grid place-items-center text-center py-28   px-6 bg-white"
+        className="grid place-items-center text-center py-12 sm:py-28   px-6 bg-white"
       >
         {/* === Top Main Image (JOIN US + Figures) === */}
         <div className="grid place-items-center w-full">
@@ -39,11 +39,11 @@ const App = () => {
         {/* === Memberships Button === */}
         <div className="grid gap-2  mt-10">
           <div className='relative'>
-            <button className="bg-yellow-400  text-white font-semibold px-10 py-4 md:px-24 md:py-6 rounded-2xl border-2 border-black shadow-lg text-lg md:text-4xl tracking-widest transition">
+            <span className="bg-yellow-400  text-white font-semibold px-10 py-4 md:px-24 md:py-6 rounded-2xl  shadow-lg text-lg md:text-4xl tracking-widest ">
               MEMBERSHIPS
-            </button>
+            </span>
             <div>
-              <img src={herocomp} alt="" className='absolute bottom-20 md:bottom-36 lg-40 left-0' srcSet="" />
+              <img src={herocomp} alt="" className='absolute h-8 w-auto   sm:h-auto sm:w-auto bottom-12 md:bottom-36 lg-40 left-0' srcSet="" />
             </div>
           </div>
           {/* === Bottom Vector Arrow === */}
@@ -68,18 +68,18 @@ const App = () => {
         <div className='container mx-auto'>
           <div className='flex flex-col justify-center items-center'>
 
-            <div className='grid md:grid-cols-2  justify-center relative'>
+            <div className='grid md:grid-cols-2  justify-center relative '>
 
               <div>
 
                 <div className='relative inline-block z-10'>
-                  <div className='mx-auto'>
-                    <a href='https://entitycollective.org/contact-us/' className="bg-cyan-600 ms-10  lg:ms-40 z-10 relative text-white text-center tracking-widest font-medium text-2xl px-16 py-3 sm:px-24 sm:py-4 sm:text-3xl rounded-2xl hover:bg-cyan-700">
+                  <div className='mx-auto -mt-32'>
+                    <a href='https://entitycollective.org/contact-us/' className="bg-cyan-600 ms-10  lg:ms-40  z-10 relative text-white text-center tracking-widest font-medium text-2xl px-16 py-3 sm:px-24 sm:py-4 sm:text-3xl border-2 border-black rounded-2xl hover:bg-cyan-700">
                       Intern
                     </a>
                   </div>
                 </div>
-                <p className='tracking-widest text-cyan-900 [word-spacing:20px] text-[15px] sm:text-[20px] mt-8'>DESIGNED FOR STUDENTS AND RECENT GRADUATES, THE INTERN MEMBERSHIP IS IDEAL FOR THOSE LOOKING TO ENGAGE IN CREATIVE, RESEARCH-BASED, OR TECHNICAL PROJECTS THAT ALIGN WITH ENTITY COLLECTIVE’S MISSION.</p>
+                <p className='tracking-widest text-cyan-900 [word-spacing:20px] text-[13px] sm:text-[20px] -mt-10'>DESIGNED FOR STUDENTS AND RECENT GRADUATES, THE INTERN MEMBERSHIP IS IDEAL FOR THOSE LOOKING TO ENGAGE IN CREATIVE, RESEARCH-BASED, OR TECHNICAL PROJECTS THAT ALIGN WITH ENTITY COLLECTIVE’S MISSION.</p>
 
 
               </div>
@@ -96,13 +96,13 @@ const App = () => {
               </div>
             </div>
             <div className='grid  md:grid-cols-2 mt-0 xl:-mt-[130px] '>
-              <div>
+              <div className='mt-5'>
                 <h3 className='text:xl sm:text-2xl text-cyan-600 font-semibold py-10'>Benifits Include:</h3>
                 <div className="p-5 bg-white">
                   <ul className="space-y-4 tracking-widest">
                     <li className="flex">
                       <span className="flex-shrink-0 mt-3.5 w-1 h-1 rounded-full bg-cyan-900 mr-3" />
-                      <p className="text-[15px] sm:text-xl leading-relaxed text-cyan-900">
+                      <p className="text-[12px] sm:text-xl leading-relaxed text-cyan-900">
                         Hands-on experience and skill development in your area of practice (art, science, economics, programming, etc.)
                       </p>
                     </li>
@@ -110,7 +110,7 @@ const App = () => {
 
                     <li className="flex">
                       <span className="flex-shrink-0 mt-3.5 w-1 h-1 rounded-full bg-cyan-900 mr-3" />
-                      <p className="text-[15px] sm:text-xl leading-relaxed text-cyan-900">
+                      <p className="text-[12px] sm:text-xl leading-relaxed text-cyan-900">
                         Opportunities for meaningful collaboration with like-minded professionals
                       </p>
                     </li>
@@ -118,7 +118,7 @@ const App = () => {
 
                     <li className="flex">
                       <span className="flex-shrink-0 mt-3.5 w-1 h-1 rounded-full bg-cyan-900 mr-3" />
-                      <p className="text-[15px] sm:text-xl leading-relaxed text-cyan-900">
+                      <p className="text-[12px] sm:text-xl leading-relaxed text-cyan-900">
                         Access to our professional network and job opportunities
                       </p>
                     </li>
@@ -126,7 +126,7 @@ const App = () => {
 
                     <li className="flex">
                       <span className="flex-shrink-0 mt-3.5 w-1 h-1 rounded-full bg-cyan-900 mr-3" />
-                      <p className="text-[15px] sm:text-xl leading-relaxed text-cyan-900">
+                      <p className="text-[12px] sm:text-xl leading-relaxed text-cyan-900">
                         20% discount on all services provided by Entity Collective
                       </p>
                     </li>
@@ -171,14 +171,14 @@ const App = () => {
 
                 <div className='relative w-full'>
                   <div className='mx-auto relative z-10'>
-                    <a href='https://entitycollective.org/contact-us/' className="bg-[#923A7F] ms-10 md:ms-0 lg:ms-40 text-white text-center tracking-widest font-medium text-2xl px-16 py-3 sm:px-24 sm:py-4 sm:text-3xl hover:bg-[#882973] rounded-2xl">
+                    <a href='https://entitycollective.org/contact-us/' className="bg-[#923A7F] ms-10 md:ms-0 lg:ms-40 text-white text-center tracking-widest font-medium text-2xl px-16 py-3 sm:px-24 sm:py-4 sm:text-3xl border-2 border-black hover:bg-[#882973] rounded-2xl">
                       Research
                     </a>
                   </div>
 
 
                 </div>
-                <p className='tracking-widest text-cyan-900 [word-spacing:20px] flex justify-center py-5 text-[15px] sm:text-[20px] mt-8'>The Researcher Membership is intended for individuals deeply involved in the arts or social sciences who are committed to conducting advanced research in collaboration with Entity projects.</p>
+                <p className='tracking-widest text-cyan-900 [word-spacing:20px] flex justify-center py-5 text-[13px] sm:text-[20px] mt-8'>The Researcher Membership is intended for individuals deeply involved in the arts or social sciences who are committed to conducting advanced research in collaboration with Entity projects.</p>
 
 
               </div>
@@ -192,7 +192,7 @@ const App = () => {
                   <ul className="space-y-4 tracking-widest">
                     <li className="flex">
                       <span className="flex-shrink-0 mt-3.5 w-1 h-1 rounded-full bg-cyan-900 mr-3" />
-                      <p className="text-[15px] sm:text-xl leading-relaxed text-cyan-900">
+                      <p className="text-[12px] sm:text-xl leading-relaxed text-cyan-900">
                         Recognition as a Researcher Member on the Entity Collective website
                       </p>
                     </li>
@@ -200,7 +200,7 @@ const App = () => {
 
                     <li className="flex">
                       <span className="flex-shrink-0 mt-3.5 w-1 h-1 rounded-full bg-cyan-900 mr-3" />
-                      <p className="text-[15px] sm:text-xl leading-relaxed text-cyan-900">
+                      <p className="text-[12px] sm:text-xl leading-relaxed text-cyan-900">
                         Access to our network of communities and partner organisations
                       </p>
                     </li>
@@ -211,7 +211,7 @@ const App = () => {
 
                     <li className="flex">
                       <span className="flex-shrink-0 mt-3.5 w-1 h-1 rounded-full bg-cyan-900 mr-3" />
-                      <p className="text-[15px] sm:text-xl leading-relaxed text-cyan-900">
+                      <p className="text-[12px] sm:text-xl leading-relaxed text-cyan-900">
                         20% discount on all services provided by Entity Collective
                       </p>
                     </li>
@@ -250,13 +250,13 @@ const App = () => {
 
                 <div className='relative inline-block z-10'>
                   <div className='mx-auto'>
-                    <a href='https://entitycollective.org/contact-us/' className="bg-[#009579] ms-10 lg:ms-32 z-10 relative text-white text-center tracking-widest font-medium text-2xl px-16 py-3 sm:px-24 sm:py-4 sm:text-3xl rounded-2xl hover:bg-[#03866e]">
+                    <a href='https://entitycollective.org/contact-us/' className="bg-[#009579] ms-10 lg:ms-32 z-10 relative text-white text-center tracking-widest font-medium text-2xl px-16 py-3 sm:px-24 sm:py-4 sm:text-3xl border-2 border-black rounded-2xl hover:bg-[#03866e]">
                       Partner
                     </a>
                   </div>
                 </div>
                 <div className='flex justify-center mx-auto'>
-                  <p className='tracking-widest  text-cyan-900 [word-spacing:20px] py-10 text-[15px] sm:text-[20px] mt-8'>DESIGNED FOR STUDENTS AND RECENT GRADUATES, THE INTERN MEMBERSHIP IS IDEAL FOR THOSE LOOKING TO ENGAGE IN CREATIVE, RESEARCH-BASED, OR TECHNICAL PROJECTS THAT ALIGN WITH ENTITY COLLECTIVE’S MISSION.</p>
+                  <p className='tracking-widest  text-cyan-900 [word-spacing:20px] py-10 text-[13px] sm:text-[20px] mt-8'>DESIGNED FOR STUDENTS AND RECENT GRADUATES, THE INTERN MEMBERSHIP IS IDEAL FOR THOSE LOOKING TO ENGAGE IN CREATIVE, RESEARCH-BASED, OR TECHNICAL PROJECTS THAT ALIGN WITH ENTITY COLLECTIVE’S MISSION.</p>
 
                 </div>
 
@@ -274,7 +274,7 @@ const App = () => {
                   <ul className="space-y-4 items-center tracking-widest">
                     <li className="flex">
                       <span className="flex-shrink-0 mt-3.5 w-1 h-1 rounded-full bg-cyan-900 mr-3" />
-                      <p className="text-[15px] sm:text-xl  leading-relaxed text-cyan-900">
+                      <p className="text-[12px] sm:text-xl  leading-relaxed text-cyan-900">
                         Hands-on experience and skill development in your area of practice (art, science, economics, programming, etc.)
                       </p>
                     </li>
@@ -282,7 +282,7 @@ const App = () => {
 
                     <li className="flex">
                       <span className="flex-shrink-0 mt-3.5 w-1 h-1 rounded-full bg-cyan-900 mr-3" />
-                      <p className="text-[15px] sm:text-xl leading-relaxed text-cyan-900">
+                      <p className="text-[12px] sm:text-xl leading-relaxed text-cyan-900">
                         Opportunities for meaningful collaboration with like-minded professionals
                       </p>
                     </li>
@@ -290,7 +290,7 @@ const App = () => {
 
                     <li className="flex">
                       <span className="flex-shrink-0 mt-3.5 w-1 h-1 rounded-full bg-cyan-900 mr-3" />
-                      <p className="text-[15px] sm:text-xl leading-relaxed text-cyan-900">
+                      <p className="text-[12px] sm:text-xl leading-relaxed text-cyan-900">
                         Access to our professional network and job opportunities
                       </p>
                     </li>
@@ -298,7 +298,7 @@ const App = () => {
 
                     <li className="flex">
                       <span className="flex-shrink-0 mt-3.5 w-1 h-1 rounded-full bg-cyan-900 mr-3" />
-                      <p className="text-[15px] sm:text-xl leading-relaxed text-cyan-900">
+                      <p className="text-[12px] sm:text-xl leading-relaxed text-cyan-900">
                         20% discount on all services provided by Entity Collective
                       </p>
                     </li>
@@ -352,7 +352,7 @@ const App = () => {
 
                 <div className='relative w-full'>
                   <div className='mx-auto relative z-10'>
-                    <a href='https://entitycollective.org/contact-us/' className="bg-[#EB8830] ms-10 md:ms-0 lg:ms-40 text-white text-center tracking-widest font-medium text-2xl px-16 py-3 sm:px-24 sm:py-4 sm:text-3xl rounded-2xl hover:bg-[#e0791e]">
+                    <a href='https://entitycollective.org/contact-us/' className="bg-[#EB8830] ms-10 md:ms-0 lg:ms-40 text-white text-center tracking-widest font-medium text-2xl px-16 py-3 sm:px-24 sm:py-4 sm:text-3xl rounded-2xl border-2 border-black hover:bg-[#e0791e]">
                       Sponsor
                     </a>
                   </div>
